fb69aa3fe20ade7c6d8dff79e64fca54de9a6222
Alireza mohammadi <alireza.mohammadi.m19@gmail.com>
2025-06-10 21:53:24 +0330

    add vazir font integration and update layout to use custom font

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import vazirFont from "@/constants/localFonts";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+      <body className={`${vazirFont} antialiased`}>{children}</body>
     </html>
   );
 }
