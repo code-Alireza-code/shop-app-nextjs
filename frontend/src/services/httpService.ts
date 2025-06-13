3b4fa341174fa71d725dabe4a43f1b648acd3d7d
Alireza mohammadi <alireza.mohammadi.m19@gmail.com>
2025-06-13 16:51:53 +0330

    add httpService for Axios requests with token refresh handling

---
+++ frontend/src/services/httpService.ts
@@ -0,0 +1,49 @@
+import axios, {
+  AxiosError,
+  AxiosRequestConfig,
+  InternalAxiosRequestConfig,
+} from "axios";
+
+interface CustomAxiosRequestConfig extends InternalAxiosRequestConfig {
+  _retry?: boolean;
+}
+
+const app = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  withCredentials: true,
+});
+
+app.interceptors.response.use(
+  (res) => res,
+  (err) => Promise.reject(err)
+);
+
+app.interceptors.request.use(
+  (res) => res,
+  async (err: AxiosError) => {
+    const originalConfig = err.config as CustomAxiosRequestConfig;
+    if (err.response?.status === 401 && !originalConfig._retry) {
+      originalConfig._retry = true;
+      try {
+        const { data } = await axios.get(
+          `${process.env.NEXT_PUBLIC_API_URL}/user/refresh-token`,
+          { withCredentials: true }
+        );
+        if (data) return app(originalConfig as AxiosRequestConfig);
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    }
+    return Promise.reject(err);
+  }
+);
+
+const http = {
+  get: app.get,
+  post: app.post,
+  patch: app.patch,
+  put: app.put,
+  delete: app.delete,
+};
+
+export default http;
