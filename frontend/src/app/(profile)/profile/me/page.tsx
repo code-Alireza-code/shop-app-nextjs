d7a0f7f00dc745d6bd60536511fa987289eca2db
Alireza mohammadi <alireza.mohammadi.m19@gmail.com>
2025-06-25 11:30:54 +0330

    add loading component + use profile page

--- frontend/src/app/(profile)/profile/me/page.tsx
+++ frontend/src/app/(profile)/profile/me/page.tsx
@@ -1,10 +1,14 @@
+import { Suspense } from "react";
 import ProfileForm from "../_/components/ProfileForm";
+import Loading from "@/ui/Loading";
 
 function page() {
   return (
     <div>
       <h1 className="font-semibold text-lg">اطلاعات کاربری</h1>
-      <ProfileForm />
+      <Suspense fallback={<Loading />}>
+        <ProfileForm />
+      </Suspense>
     </div>
   );
 }
