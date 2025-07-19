b7ebc10a58877da9844aeec163a071f86815fb6b
Alireza mohammadi <alireza.mohammadi.m19@gmail.com>
2025-07-19 21:32:30 +0330

    complete profile's dashbord page

--- frontend/src/app/(profile)/profile/page.tsx
+++ frontend/src/app/(profile)/profile/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 import { useGetUser } from "@/hooks/useAuth";
 import toLocaleDateString from "@/utils/dateFormatter";
+import PaymentTable from "./_/components/PaymentTable";
+import Link from "next/link";
 
 function ProfilePage() {
-  const { user, isLaodingUser } = useGetUser();
+  const { user, payments, isLaodingUser } = useGetUser();
 
   if (isLaodingUser) return <p>Loading...</p>;
   return (
@@ -17,6 +19,25 @@ function ProfilePage() {
           day: "numeric",
         })}
       </span>
+      <div className="border rounded-xl p-4 mt-8">
+        <div className="flex items-center justify-between">
+          <h2 className="font-semibold">آخرین سفارشات کاربر</h2>
+          <Link
+            href="/profile/payments"
+            className="text-primary-900 hover:underline"
+          >
+            مشاهده همه سفارشات
+          </Link>
+        </div>
+        <PaymentTable
+          payments={payments
+            .sort(
+              (a, b) =>
+                Number(new Date(b.createdAt)) - Number(new Date(a.createdAt))
+            )
+            .slice(0, 1)}
+        />
+      </div>
     </div>
   );
 }
