06a9817b146fe24925cbca96243c3087bf34f42a
Alireza mohammadi <alireza.mohammadi.m19@gmail.com>
2025-08-10 21:10:21 +0330

    add a simple home page

--- frontend/src/app/(user)/page.tsx
+++ frontend/src/app/(user)/page.tsx
@@ -1,3 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div className="text-xl">صفحه خانه</div>;
+  return (
+    <div className="flex flex-col gap-y-4 items-center justify-center">
+      <h1 className="text-5xl mt-8 font-semibold">فروشگاه نکس شاپ</h1>
+      <p className="text-lg">
+        اینجا میتونی تمام وسایل الکترونیکی دلخواهت رو با بهترین قیمت بخری😉
+      </p>
+      <Link href="/products">
+        <button className="btn btn--primary ">بزن بریم</button>
+      </Link>
+    </div>
+  );
 }
