dda816b30e63de1f8aadc62e02d54d0679997262
Alireza mohammadi <alireza.mohammadi.m19@gmail.com>
2025-08-10 11:39:04 +0330

    remove access to /auth after signin

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -15,8 +15,13 @@ export default async function middleware(request: NextRequest) {
     if (!user) return NextResponse.redirect(new URL("/auth", request.url));
     console.log("/profile");
   }
+
+  if (request.nextUrl.pathname.startsWith("/auth")) {
+    const { user } = await middlewareAuth(request);
+    if (user) return NextResponse.redirect(new URL("/", request.url));
+  }
 }
 
 export const config = {
-  matcher: ["/admin/:path*", "/profile/:path*"],
+  matcher: ["/admin/:path*", "/profile/:path*", "/auth/:path*"],
 };
